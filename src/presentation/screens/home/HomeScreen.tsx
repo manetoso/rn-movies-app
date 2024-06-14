566e3ef6575956c1c794b99c61c44c66795608fe
manetoso <manetoso98@hotmail.com>
2024-06-14 15:20:58 -0600

    feat(screens) add ui in Home

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,20 +1,42 @@
 import React, { FC } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import { useMovies } from '@/presentation/hooks/useMovies';
+import { PosterCarousel } from '@/presentation/components/movies/PosterCarousel';
+import { HorizontalCarousel } from '@/presentation/components/movies/HorizontalCarousel';
 
 interface Props {}
 
 export const HomeScreen: FC<Props> = () => {
-	return (
-		<View style={styles.container}>
-			<Text>HomeScreen</Text>
-		</View>
-	);
+  const { top } = useSafeAreaInsets();
+  const { isLoading, nowPlaying, populars, topRated, upcoming } = useMovies();
+
+  if (isLoading) {
+    return (
+      <View style={[styles.container, { marginTop: top + 20 }]}>
+        <Text>Cargando...</Text>
+      </View>
+    );
+  }
+  return (
+    <ScrollView>
+      <View style={[styles.container, { marginTop: top + 20 }]}>
+        <PosterCarousel movies={nowPlaying} />
+        <HorizontalCarousel
+          movies={populars}
+          title="Populares"
+          loadNextPage={() => console.log('Cargando mas...')}
+        />
+        <HorizontalCarousel movies={topRated} title="Mejor Calificadas" />
+        <HorizontalCarousel movies={upcoming} title="Proximamente" />
+      </View>
+    </ScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
+  container: {
+    paddingBottom: 30,
+  },
 });
